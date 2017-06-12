e5b0a2310b088a914b5d1851e39aefb17ef324e9
Tatyana Volkorezova <tatyana.volkorezova@onix-systems.com>
2017-06-12 20:07:06 +0300

    add initial rows

--- src/TestOutput.java
+++ src/TestOutput.java
@@ -7,6 +7,18 @@ public class TestOutput {
 
         System.out.println("Hello");
 
+        int var1=15;
+
+        int var2 = 20;
+        String varString = "test";
+
+        System.out.println(var1 + " "+varString+" "+ var2);
+
+        int rez_1 = var1+var2;
+        System.out.println("Rez1 = "+rez_1);
+
+
+
     }
-   
+
 }
