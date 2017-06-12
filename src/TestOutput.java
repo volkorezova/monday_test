2f5794148d2274896f101cfee4312ceef101eb7c
Tatyana Volkorezova <tatyana.volkorezova@onix-systems.com>
2017-06-12 10:18:22 +0300

    create idea project

---
+++ src/TestOutput.java
@@ -0,0 +1,14 @@
+/**
+ * Created by tatyanavolkorezova on 08.06.17.
+ */
+public class TestOutput {
+
+    public static void main(String arg[]){
+
+        System.out.println("Hello");
+        wkjryhmsfdlwersymhdfgwlearksmydfgbcla
+    }
+    esfdhbkerjfns
+
+    jkzedfhxhbzosiekzduxhfjvnzsirluk
+}
